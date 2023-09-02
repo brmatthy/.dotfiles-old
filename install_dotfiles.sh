84de9db63c39156717ac0bc7d73e22ba519a95b7
brmatthy <109791839+brmatthy@users.noreply.github.com>
2023-09-02 12:13:37 +0200

    setup .gitconfig

---
+++ install_dotfiles.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+# make symlink to nvim config
+ln -s $HOME/.dotfiles/nvim/ ~/.config/
+
+# make symlink to .gitconfig file
+ln -s $HOME/.dotfiles/.gitconfig ~/.gitconfig
---
+++ programs/install_arch.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# Help message
+help_message() {
+    cat $HOME/.dotfiles/programs/help.txt
+}
+
+# Install Bitwarden
+install_bitwarden() {
+    sudo snap install bitwarden
+}
+
+# Install Discord
+install_discord() {
+    sudo snap install discord
+}
+
+# Install Firefox
+install_firefox() {
+    sudo pacman -S firefox
+}
+
+# Install Git
+install_git() {
+    sudo pacman -S git
+}
+
+# Install Joplin
+install_joplin() {
+    wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
+}
+
+# Install Neofetch
+install_neofetch() {
+    sudo pacman -S neofetch
+}
+
+# Install Nvim
+install_nvim() {
+    sudo pacman -S neovim
+}
+
+# Install OneDrive CLI Client
+install_onedrive() {
+}
+
+# Install Pop_OS! Shell
+install_pop_shell() {
+}
+
+
+# Install Spicetify
+install_spicetify() {
+    # Add install command for Spicetify
+}
+
+# Install Spotify
+install_spotify() {
+    # Add install command for Spotify
+}
+
+# Main script logic
+if [ $# -eq 0 ]; then
+    help_message
+else
+    for arg in "$@"; do
+        case $arg in
+            -h|--help)
+                help_message
+                ;;
+            -a|--all)
+                # Call functions for installing all programs
+                ;;
+            -b|--bitwarden)
+                install_bitwarden
+                ;;
+            -d|--discord)
+                install_discord
+                ;;
+            -f|--firefox)
+                install_firefox
+                ;;
+            -g|--git)
+                install_git
+                ;;
+            -j|--joplin)
+                install_joplin
+                ;;
+            --neofetch)
+                install_neofetch
+                ;;
+            --nvim)
+                install_nvim
+                ;;
+            -o|--onedrive)
+                install_onedrive
+                ;;
+            -p|--pop-shell)
+                install_pop_shell
+                ;;
+            --spicetify)
+                install_spicetify
+                ;;
+            --spotify)
+                install_spotify
+                ;;
+            *)
+                echo "Unknown option: $arg"
+                ;;
+        esac
+    done
+fi
