d04863a943c578f6fe559e32151c866e7ab792ea
brmatthy <109791839+brmatthy@users.noreply.github.com>
2023-09-05 21:21:40 +0200

    add cursor theme

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+# make symlink to icons dir
+ln -s $HOME/.dotfiles/.local/share/icons/ $HOME/.local/share/
+
 # make symlink to nvim config dir
 ln -s $HOME/.dotfiles/.config/nvim/ $HOME/.config/
 
@@ -14,6 +17,13 @@ ln -s $HOME/.dotfiles/.local/share/fonts/ $HOME/.local/share/
 
 # make symlink to rofi config dir
 ln -s $HOME/.dotfiles/.config/rofi/ $HOME/.config/
+
+# install gtk theme
+yay -S catppuccin-gtk-theme-frappe
+mkdir -p "${HOME}/.config/gtk-4.0"
+ln -sf "${THEME_DIR}/gtk-4.0/assets" "${HOME}/.config/gtk-4.0/assets"
+ln -sf "${THEME_DIR}/gtk-4.0/gtk.css" "${HOME}/.config/gtk-4.0/gtk.css"
+ln -sf "${THEME_DIR}/gtk-4.0/gtk-dark.css" "${HOME}/.config/gtk-4.0/gtk-dark.css"
 
 # set the gnome keybinds
 ./util/gsettings.sh
