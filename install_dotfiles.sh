599cc4827c0ec20243ab1bd773f55dcdd78caf7d
brmatthy <109791839+brmatthy@users.noreply.github.com>
2023-09-08 10:17:38 +0200

    fix download theme

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -21,12 +21,5 @@ ln -s $HOME/.dotfiles/.config/alacritty/ $HOME/.config/
 # make symlink to rofi config dir
 ln -s $HOME/.dotfiles/.config/rofi/ $HOME/.config/
 
-# install gtk theme
-yay -S catppuccin-gtk-theme-frappe
-mkdir -p "${HOME}/.config/gtk-4.0"
-ln -sf "${THEME_DIR}/gtk-4.0/assets" "${HOME}/.config/gtk-4.0/assets"
-ln -sf "${THEME_DIR}/gtk-4.0/gtk.css" "${HOME}/.config/gtk-4.0/gtk.css"
-ln -sf "${THEME_DIR}/gtk-4.0/gtk-dark.css" "${HOME}/.config/gtk-4.0/gtk-dark.css"
-
 # set the gnome keybinds
 ./util/gsettings.sh
