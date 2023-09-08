fb6a494c88f979591b1ec05ab5955d90e8a84dc0
brmatthy <109791839+brmatthy@users.noreply.github.com>
2023-09-08 11:24:16 +0200

    use ulauncher as app launcher

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -18,8 +18,11 @@ ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
 # make symlink to alacritty config dir
 ln -s $HOME/.dotfiles/.config/alacritty/ $HOME/.config/
 
-# make symlink to rofi config dir
-ln -s $HOME/.dotfiles/.config/rofi/ $HOME/.config/
+# make symlink to nvim config dir
+ln -s $HOME/.dotfiles/.config/nvim/ $HOME/.config/
+
+# make symlink to ulauncher config dir
+ln -s $HOME/.dotfiles/.config/ulauncher/ $HOME/.config/
 
 # set the gnome keybinds
 ./util/gsettings.sh
