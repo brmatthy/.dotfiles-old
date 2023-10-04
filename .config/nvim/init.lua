b8e23a65be057ce288b3d2d1cb747c634486cb25
brmatthy <109791839+brmatthy@users.noreply.github.com>
2023-10-04 16:36:31 +0200

    upgraded nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,11 +6,17 @@
 -- │░░░░░░░░░░░░Brent░Matthys░on░01-10-2023░░░░░░░░░░░░│
 -- └───────────────────────────────────────────────────┘
 
--- make :e be in working directory
-vim.cmd("set autochdir")
-
 -- set relative line numbers
 vim.cmd("set rnu")
 
 -- require the plugins
 require("plugins")
+
+-- require the core
+require("core")
+
+-- Set highlight on search
+vim.o.hlsearch = false
+
+-- Set yank buffer as clipboard
+vim.cmd("set clipboard+=unnamedplus")
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -32,13 +32,35 @@ require("lazy").setup({
   -- All plugins without configuration --
   ---------------------------------------
 
+  -- Add indentation guides even on blank lines
+   { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
 
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  -- plugin for coq theorem prover
+  'whonore/Coqtail',
   --------------------------------
   -- Plugins with configuration --
   --------------------------------
@@ -58,6 +80,63 @@ require("lazy").setup({
     build = ':TSUpdate',
   },
 
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
+  },
+
+  -- Set status line
+  { 'nvim-lualine/lualine.nvim' },
+
+  -- Fuzzy finder
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+  },
+
+  {
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      { 'williamboman/mason.nvim', config = true },
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+    },
+  },
+
 })
 
 ----------------------------
@@ -73,3 +152,15 @@ vim.cmd.colorscheme 'catppuccin'
 
 -- import treesitter config
 require "plugins.configs.treesitter"
+
+-- import autocomplete config
+require "plugins.configs.nvim-cmp"
+
+-- import lualine config
+require "plugins.configs.lualine"
+
+-- import telescope config
+require "plugins.configs.telescope"
+
+-- import lsp config
+require "plugins.configs.lspconfig"
