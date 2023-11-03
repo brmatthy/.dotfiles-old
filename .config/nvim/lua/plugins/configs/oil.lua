5b81f5a77370b1ff5086498474f85906fa45d2cb
brmatthy <109791839+brmatthy@users.noreply.github.com>
2023-11-03 19:57:44 +0100

    nvim: oil open floating

--- .config/nvim/lua/plugins/configs/oil.lua
+++ .config/nvim/lua/plugins/configs/oil.lua
@@ -136,4 +136,4 @@ require("oil").setup({
   },
 })
 
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
